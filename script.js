05b81ff6bf4ebab3c0b24181c4cbc990cc46da09
anuveyatsu <anuar.ustayev@gmail.com>
2019-02-06 12:48:25 +0600

    [views][m]: added view for GVA

--- script.js
+++ script.js
@@ -2,6 +2,49 @@ var DP_ID = {
   "name": "london-dashboard",
   "resources": [
     {
+      "encoding": "utf-8",
+      "format": "json",
+      "mediatype": "text/xls",
+      "name": "gva",
+      "path": "https://datahub.io/london/gva/r/gva.json",
+      "profile": "tabular-data-resource",
+      "quotechar": "\"",
+      "schema": {
+        "fields": [
+          {
+            "format": "default",
+            "name": "NUTS level",
+            "type": "string"
+          },
+          {
+            "format": "default",
+            "name": "NUTS code",
+            "type": "string"
+          },
+          {
+            "format": "default",
+            "name": "Region name",
+            "type": "string"
+          },
+          {
+            "format": "default",
+            "name": "Year",
+            "type": "year"
+          },
+          {
+            "decimalChar": ".",
+            "format": "default",
+            "groupChar": "",
+            "name": "Value",
+            "type": "number"
+          }
+        ],
+        "missingValues": [
+          ""
+        ]
+      }
+    },
+    {
       "encoding": "ISO-8859-1",
       "format": "csv",
       "mediatype": "text/csv",
@@ -303,6 +346,89 @@ var DP_ID = {
 
 
 DP_ID.views = [
+  {
+    "resources": ["gva"],
+    "specType": "vega",
+    "spec": {
+      "$schema": "https://vega.github.io/schema/vega/v3.json",
+      "width": 228,
+      "height": 160,
+      "padding": 0,
+      "data": [
+        {
+          "name": "gva",
+          "format": {
+            "parse": {
+              "Year": "date"
+            }
+          },
+          "transform": [
+            {
+              "type": "filter",
+              "expr": "datum['Region name'] == 'London'"
+            }
+          ]
+        }
+      ],
+      "scales": [
+        {
+          "name": "x",
+          "type": "utc",
+          "range": "width",
+          "domain": {
+            "data": "gva",
+            "field": "Year"
+          }
+        },
+        {
+          "name": "value",
+          "type": "linear",
+          "range": "height",
+          "zero": false,
+          "domain": {
+            "data": "gva",
+            "field": "Value"
+          }
+        }
+      ],
+      "axes": [
+        {
+          "orient": "bottom",
+          "scale": "x",
+          "labelFont": "Lato",
+          "format": "%Y",
+          "domain": false,
+          "ticks": false,
+          "labelPadding": 10
+        },
+        {
+          "orient": "right",
+          "scale": "value",
+          "labelFont": "Lato",
+          "format": "s",
+          "grid": true,
+          "domain": false,
+          "ticks": false,
+          "title": "GBP",
+          "titleFontWeight": "light"
+        }
+      ],
+      "marks": [
+        {
+          "type": "line",
+          "from": {"data": "gva"},
+          "encode": {
+            "enter": {
+              "x": {"scale": "x", "field": "Year"},
+              "y": {"scale": "value", "field": "Value"},
+              "strokeWidth": {"value": 2},
+              "stroke": {"value": "#A95F6D"}
+            }
+          }
+        }
+      ]
+    }
+  },
   {
     "resources": [
       "unemployment-rate"
