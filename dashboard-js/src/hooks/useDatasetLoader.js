515ebfc2604d8db29c76034d3e47bac745273e42
anuveyatsu <anuar.ustayev@gmail.com>
2019-02-17 22:43:58 +0600

    [dataset loader][s]: work with data.js + fixes for 'useDatasetLoader' - refs #10

--- dashboard-js/src/hooks/useDatasetLoader.js
+++ dashboard-js/src/hooks/useDatasetLoader.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 const {Dataset} = require('data.js');
+const toArray = require('stream-to-array')
 
 
 function useDatasetLoader(identifiersOrResources) {
@@ -8,32 +9,26 @@ function useDatasetLoader(identifiersOrResources) {
 
   useEffect(() => {
     // Load datasets:
-    config.datasets.forEach(identifier => {
+    config.datasets.forEach(async identifier => {
       // Check if this dataset is already loaded:
       if (!currentDatasets.some(dataset => dataset.identifier.original === identifier)) {
         // Load the dataset:
-        Dataset.load(identifier)
-          .then(dataset => {
-            // Load/compile resources:
-            dataset.resources.map(async resource => {
-              // Load it here
-            })
-            // Create a new list of datasets by including a new one:
-            const newDatasets = []
-            if (Array.isArray(currentDatasets)) {
-              newDatasets.concat(currentDatasets)
-              newDatasets.push(dataset)
-            }
-            addDataset(newDatasets)
-            return currentDatasets
-          })
+        const dataset = await Dataset.load(identifier)
+        // Load/compile resources:
+        await Promise.all(dataset.resources.map(async resource => {
+          // Load it here
+          const rowStream = await resource.rows({keyed: true})
+          resource.descriptor._values = await toArray(rowStream)
+        }))
+        // Create a new list of datasets by including a new one:
+        const newDatasets = []
+        if (Array.isArray(currentDatasets)) {
+          newDatasets.concat(currentDatasets)
+          newDatasets.push(dataset)
+        }
+        addDataset(newDatasets)
+        return currentDatasets
       }
-    })
-
-    // Find required dataset and return it:
-    identifiersOrResources.forEach(resource => {
-      const dataset = currentDatasets.find(dataset => dataset.name === resource.datasetId)
-      return dataset
     })
   })
 
