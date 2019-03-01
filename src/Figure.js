7e9eef4b467aaaf41d6b0006c0a76b68649c8108
anuveyatsu <anuar.ustayev@gmail.com>
2019-03-01 17:25:39 +0600

    [Figure][s]: widget spec should be minimal and use transforms - refs #5

--- src/Figure.js
+++ src/Figure.js
@@ -2,33 +2,37 @@ import React from 'react';
 
 
 function Figure(props) {
-  let value
-  switch (props.compiledView.spec.operation) {
-    case 'show-first-data':
-      if (props.compiledView.resources[0]._values) {
-        const firstRow = props.compiledView.resources[0]._values[0]
-        value = firstRow[props.compiledView.spec.fieldName]
-      }
-      break
-    case 'show-latest-data':
-    default:
-      if (props.compiledView.resources[0]._values) {
-        const length = props.compiledView.resources[0]._values.length
-        const lastRow = props.compiledView.resources[0]._values[length - 1]
-        value = lastRow[props.compiledView.spec.fieldName]
-      }
-      break
+  let fieldName = props.compiledView.spec.fieldName
+  // We expect that after transformation, the `_values` array will have single row:
+  let values = props.compiledView.resources[0]._values[0]
+  // Flatten the values:
+  Object.keys(values).forEach(key => {
+    let value = values[key]
+    if (value.constructor.name === 'Object') {
+      Object.keys(value).forEach(anotherKey => {
+        values[anotherKey] = value[anotherKey]
+      })
+    }
+  })
+  // Take the value we need:
+  let output = values[fieldName]
+  // If it's a number, use only 1 decimal place:
+  if (!isNaN(output)) {
+    output = parseFloat(output).toFixed(1).toString()
   }
-  
+
   return (
     <>
-      <h2>{props.compiledView.spec.title}</h2>
+      <h2>{props.compiledView.title}</h2>
       <div className='info'>
-        <span>{parseFloat(value).toFixed(1) + props.compiledView.spec.suffix}</span> <br />
-        <h4>{props.compiledView.spec.legend}</h4>
+        <span>
+          {props.compiledView.spec.prefix || '' + output + props.compiledView.spec.suffix || ''}
+        </span>
+        <br />
+        <h4>{props.compiledView.legend}</h4>
       </div>
 
-      <p>{props.compiledView.spec.footer}</p>
+      <p>{props.compiledView.footer}</p>
     </>
   )
 }
